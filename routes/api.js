251221cdce7a781c0d13c8a581bed1a0ff201197
Thonatos.Yang <thonatos.yang@gmail.com>
2016-03-23 14:13:10 +0800

    调整群聊中只有被@的时候才会回复消息

--- routes/api.js
+++ routes/api.js
@@ -8,106 +8,112 @@ const router = express.Router()
 let botInstanceArr = {}
 
 router.get('/uuid', (req, res) => {
-  let bot = new WxBot()
+    
+    let bot = new WxBot()
 
-  bot.getUUID()
-    .then(uuid => {
-      res.send(uuid)
-      botInstanceArr[uuid] = bot
-      
-      debug('新连接', Object.getOwnPropertyNames(botInstanceArr).length)
-    })
-    .catch(err => {
-      res.sendStatus(404)
-      debug('获取UUID失败')
-    })
+    bot.getUUID()
+        .then(uuid => {
+            res.send(uuid)
+            botInstanceArr[uuid] = bot
+
+            debug('新连接', Object.getOwnPropertyNames(botInstanceArr).length)
+        })
+        .catch(err => {
+            res.sendStatus(404)
+            debug('获取UUID失败')
+        })
 })
 
 router.get('/instance/:uuid', (req, res) => {
-  let bot = botInstanceArr[req.params.uuid]
+    
+    let bot = botInstanceArr[req.params.uuid]
 
-  debug(req.params.uuid, !!bot)
-  if (bot && bot.state === WxBot.STATE.login) {
-    res.sendStatus(200)
-  } else {
-    res.sendStatus(404)
-  }
+    debug(req.params.uuid, !!bot)
+    
+    if (bot && bot.state === WxBot.STATE.login) {
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(404)
+    }
 
 })
 
 router.get('/login/:uuid', (req, res) => {
-  let bot = botInstanceArr[req.params.uuid]
+    let bot = botInstanceArr[req.params.uuid]
 
-  bot.start()
-    .then(() => {
-      res.sendStatus(200)
-      bot.on('logout', () => {
-          delete botInstanceArr[req.params.uuid]
-          
-          debug('关闭注销连接', Object.getOwnPropertyNames(botInstanceArr).length)
+    bot.start()
+        .then(() => {
+            res.sendStatus(200)
+            bot.on('logout', () => {
+                delete botInstanceArr[req.params.uuid]
+
+                debug('关闭注销连接', Object.getOwnPropertyNames(botInstanceArr).length)
+            })
         })
-    })
-    .catch(err => {
-      delete botInstanceArr[req.params.uuid]
-      res.sendStatus(403)
-      
-      debug('关闭登陆失败连接', Object.getOwnPropertyNames(botInstanceArr).length)
-    })
+        .catch(err => {
+            delete botInstanceArr[req.params.uuid]
+            res.sendStatus(403)
+
+            debug('关闭登陆失败连接', Object.getOwnPropertyNames(botInstanceArr).length)
+        })
 
 })
 
 router.get('/autoReplyList/:uuid', (req, res) => {
-  let bot = botInstanceArr[req.params.uuid]
-  
-  if (bot && bot.state === WxBot.STATE.login) {
-    res.send(bot.autoReplyList())
-  } else {
-    res.sendStatus(404)
-  }
+    let bot = botInstanceArr[req.params.uuid]
+
+    if (bot && bot.state === WxBot.STATE.login) {
+        res.send(bot.autoReplyList())
+    } else {
+        res.sendStatus(404)
+    }
 
 })
 
 router.get('/superviseList/:uuid', (req, res) => {
-  let bot = botInstanceArr[req.params.uuid]
-  
-  if (bot && bot.state === WxBot.STATE.login) {
-    res.send(bot.superviseList())
-  } else {
-    res.sendStatus(404)
-  }
+    
+    let bot = botInstanceArr[req.params.uuid]
+
+    if (bot && bot.state === WxBot.STATE.login) {
+        res.send(bot.superviseList())
+    } else {
+        res.sendStatus(404)
+    }
 
 })
 
 router.get('/autoReply/:uuid/:uid', (req, res) => {
-  let bot = botInstanceArr[req.params.uuid]
+    
+    let bot = botInstanceArr[req.params.uuid]
 
-  if (bot.replyUsers.has(req.params.uid)) {
-    bot.replyUsers.delete(req.params.uid)
-    bot.sendMsg("主人关闭了我，拜拜了！", req.params.uid)
-    
-    debug('删除自动回复用户', req.params.uid)
-  } else {
-    bot.replyUsers.add(req.params.uid)
-    
-    debug('增加自动回复用户', req.params.uid)
-  }
-  res.sendStatus(200)
+    if (bot.replyUsers.has(req.params.uid)) {
+        bot.replyUsers.delete(req.params.uid)
+        bot.sendMsg("我要回去搬砖了，各位ヾ(￣▽￣)Bye~Bye~", req.params.uid)
+
+        debug('删除自动回复用户', req.params.uid)
+    } else {
+        bot.replyUsers.add(req.params.uid)
+
+        debug('增加自动回复用户', req.params.uid)
+    }
+    res.sendStatus(200)
 
 })
 
 router.get('/supervise/:uuid/:uid', (req, res) => {
-  let bot = botInstanceArr[req.params.uuid]
+    
+    let bot = botInstanceArr[req.params.uuid]
 
-  if (bot.superviseUsers.has(req.params.uid)) {
-    bot.superviseUsers.delete(req.params.uid)
-    
-    debug('删除监督用户', req.params.uid)
-  } else {
-    bot.superviseUsers.add(req.params.uid)
-    
-    debug('增加监督用户', req.params.uid)
-  }
-  res.sendStatus(200)
+    if (bot.superviseUsers.has(req.params.uid)) {
+        bot.superviseUsers.delete(req.params.uid)
+
+        debug('删除监督用户', req.params.uid)
+    } else {
+        bot.superviseUsers.add(req.params.uid)
+
+        debug('增加监督用户', req.params.uid)
+    }
+    res.sendStatus(200)
 
 })
 
